9450cf48118d0b6ed90ea1f5cde16d7fca192528
Lucas Holmquist <lholmqui@redhat.com>
2013-09-04 21:53:00 -0400

    Add Some Tests

--- test/client_sender_test.js
+++ test/client_sender_test.js
@@ -1,10 +1,145 @@
-var assert = require("assert");
+var chai = require( "chai" ),
+    sinonChai = require( "sinon-chai" ),
+    nock = require( "nock" ),
+    expect = require( "chai" ).expect,
+    AeroGear = require( "../lib/aerogear-sender-client" ).AeroGear,
+    server;
 
-describe('Array', function(){
-  describe('#indexOf()', function(){
-    it('should return -1 when the value is not present', function(){
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
+chai.use( sinonChai );
+
+describe( "Sender", function() {
+    describe( "Create", function() {
+        it( "Sender should be an object", function() {
+            var sender = AeroGear.Sender();
+            expect( sender ).to.be.an( "object" );
+        });
+
+        it( "Sender should inherit events", function() {
+            var sender = AeroGear.Sender();
+            expect( sender.emit ).to.exist;
+        });
+
+        it( "Sender should have a broadcast method", function() {
+            var sender = AeroGear.Sender();
+            expect( sender.broadcast ).to.exist;
+        });
+
+        it( "Sender should have a sendTo method", function() {
+            var sender = AeroGear.Sender();
+            expect( sender.sendTo ).to.exist;
+        });
+
+        it( "Sender should have a URL contructed", function() {
+            var sender = AeroGear.Sender(
+                {
+                    baseURL: "http://localhost:8080/ag-push",
+                    endpoint: "/sender"
+                });
+            expect( sender.getUrl() ).to.equal( "http://localhost:8080/ag-push/sender" );
+        });
     });
-  });
+});
+
+describe( "Sender Broadcast", function() {
+    var sender = AeroGear.Sender(
+                {
+                    baseURL: "http://localhost:8080/ag-push",
+                    endpoint: "/sender/"
+                }),
+            applicationID = "12345",
+            masterSecret = "54321",
+            settings = {},
+            message = {};
+
+    beforeEach( function() {
+        settings = {};
+        message = {};
+    });
+
+
+    describe( "Broadcast Method", function() {
+        it( "broadcast should be called with success", function( done ) {
+
+            nock( "http://localhost:8080" )
+            .matchHeader('Accept', 'application/json')
+            .matchHeader('Content-type', 'application/json')
+            .post( "/ag-push/sender/broadcast" )
+            .reply( 200,{} );
+
+            settings.applicationID = applicationID;
+            settings.masterSecret = masterSecret;
+
+            sender.broadcast( message, settings ).on( "success", function( response ) {
+                expect( response ).to.be.ok;
+                done();
+            });
+        });
+
+        it( "broadcast should be called with error", function( done ) {
+            nock( "http://localhost:8080" )
+            .matchHeader('Accept', 'application/json')
+            .matchHeader('Content-type', 'application/json')
+            .post( "/ag-push/sender/broadcast" ).reply( 400,{} );
+
+            settings.applicationID = applicationID;
+            settings.masterSecret = masterSecret;
+
+            sender.broadcast( message, settings ).on( "error", function( error ) {
+                expect( error ).to.be.ok;
+                done();
+            });
+        });
+    });
+});
+
+describe( "Sender - sendTo", function() {
+    var sender = AeroGear.Sender(
+                {
+                    baseURL: "http://localhost:8080/ag-push",
+                    endpoint: "/sender/"
+                }),
+            applicationID = "12345",
+            masterSecret = "54321",
+            settings = {},
+            message = {};
+
+    beforeEach( function() {
+        settings = {};
+        message = {};
+    });
+
+
+    describe( "sendTo Method", function() {
+        it( "broadcast should be called with success", function( done ) {
+
+            nock( "http://localhost:8080" )
+            .matchHeader('Accept', 'application/json')
+            .matchHeader('Content-type', 'application/json')
+            .post( "/ag-push/sender/selected" )
+            .reply( 200,{} );
+
+            settings.applicationID = applicationID;
+            settings.masterSecret = masterSecret;
+
+            sender.sendTo( message, settings ).on( "success", function( response ) {
+                expect( response ).to.be.ok;
+                done();
+            });
+        });
+
+        it( "broadcast should be called with error", function( done ) {
+            nock( "http://localhost:8080" )
+            .matchHeader('Accept', 'application/json')
+            .matchHeader('Content-type', 'application/json')
+            .post( "/ag-push/sender/selected" ).reply( 400,{} );
+
+            settings.applicationID = applicationID;
+            settings.masterSecret = masterSecret;
+
+            sender.sendTo( message, settings ).on( "error", function( error ) {
+                expect( error ).to.be.ok;
+                done();
+            });
+        });
+    });
 });
