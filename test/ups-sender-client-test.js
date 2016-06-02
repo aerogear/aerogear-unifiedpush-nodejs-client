b8b825fab3f4c61cf511a732575c6c567c9500fe
Lucas Holmquist <lholmqui@redhat.com>
2016-06-02 15:58:57 -0400

    add more tests for the ups sender client

--- test/ups-sender-client-test.js
+++ test/ups-sender-client-test.js
@@ -16,6 +16,64 @@ test('test senderClient should return a Promise with the client object', (t) => 
     upsSenderClient.then((client) => {
         t.equal(typeof client.url, 'string', 'client should have a url String');
         t.equal(client.url, 'http://localhost:8080/ag-push/rest/sender/', 'client should have a url value of settings.url');
+
+        t.equals(typeof client.sender.send, 'function', 'should have the send function');
+        t.end();
+    });
+});
+
+test('test senderClient url with trailing slash', (t) => {
+    const settings = {
+        url: 'http://localhost:8080/ag-push/',
+        applicationId: '54321',
+        masterSecret: 'secret'
+    };
+
+    agSender(settings).then((client) => {
+        t.equal(client.url, 'http://localhost:8080/ag-push/rest/sender/', 'client should have a url value of settings.url');
+        t.end();
+    });
+});
+
+test('test senderClient should reject when no settings', (t) => {
+    agSender().catch((err) => {
+        t.equal(err, 'UnifiedPushSenderError', 'should return a promise rejection when missing the applicationId');
+        t.end();
+    });
+});
+
+test('test senderClient should reject when applicationId setting', (t) => {
+    const settings = {
+        url: 'http://localhost:8080/ag-push',
+        masterSecret: 'secret'
+    };
+
+    agSender(settings).catch((err) => {
+        t.equal(err, 'UnifiedPushSenderError', 'should return a promise rejection when missing the applicationId');
+        t.end();
+    });
+});
+
+test('test senderClient should reject when masterSecret setting', (t) => {
+    const settings = {
+        url: 'http://localhost:8080/ag-push',
+        applicationId: '54321'
+    };
+
+    agSender(settings).catch((err) => {
+        t.equal(err, 'UnifiedPushSenderError', 'should return a promise rejection when missing the masterSecert');
+        t.end();
+    });
+});
+
+test('test senderClient should reject when url setting', (t) => {
+    const settings = {
+        applicationId: '54321',
+        masterSecret: 'secret'
+    };
+
+    agSender(settings).catch((err) => {
+        t.equal(err, 'UnifiedPushSenderError', 'should return a promise rejection when missing the url');
         t.end();
     });
 });
