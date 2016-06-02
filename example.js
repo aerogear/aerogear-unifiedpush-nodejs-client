d978c08e344dd12bd204b2cb1f18bb3698118f17
Lucas Holmquist <lholmqui@redhat.com>
2016-06-02 15:13:56 -0400

    initial re-write of the sender lib.

--- example.js
+++ example.js
@@ -1,75 +1,65 @@
-var agSender = require( "./lib/unifiedpush-node-sender" ),
-    message,
-    settings,
-    options;
+const agSender = require('./');
 
-//Send a Message
-settings = require( "./settings.json" );
+const settings = require('./settings.json');
 
-message = {
-    alert: "Hi",
-    sound: "default",
+const message = {
+    alert: 'Hi',
+    sound: 'default',
     badge: 2,
-    simplePush: "version=123",
+    simplePush: 'version=123',
     userData: {
-        someKey: "some value",
-        anotherCustomKey: "some other value",
+        someKey: 'some value',
+        anotherCustomKey: 'some other value',
     }
 };
 
-options = {
+const options = {
     config: {
         ttl: 3600,
     },
     criteria: {
-        variants: [ "1234", "56788" ],
-        categories: [ "category1", "category2" ]
+        variants: ['1234', '56788'],
+        categories: ['category1', 'category2']
     }
 };
 
-agSender.Sender( settings ).send( message, options )
-    .on( "success", function( response ) {
-        console.log( "success called", response );
-    })
-    .on( "error", function( err ) {
-        console.log( err );
+agSender(settings).then((client) => {
+    return client.send(message, options).then((result) => {
+        console.log(result);
     });
+});
 
 
 // A slightly more complex message example
-
-message = {
-    "alert": "HELLO!",
-    "sound": "default",
-    "badge": 2,
-    "user-data": {
-        "key": "value",
-        "key2": "other value"
+const message2 = {
+    'alert': 'HELLO!',
+    'sound': 'default',
+    'badge': 2,
+    'user-data': {
+        'key': 'value',
+        'key2': 'other value'
     },
-    "windows": {
-        "type": "tile",
-        "duration": "short",
-        "badge": "alert",
-        "tileType": "TileWideBlockAndText01",
-        "images": ["Assets/test.jpg", "Assets/background.png"],
-        "textFields": ["foreground text"]
+    'windows': {
+        'type': 'tile',
+        'duration': 'short',
+        'badge': 'alert',
+        'tileType': 'TileWideBlockAndText01',
+        'images': ['Assets/test.jpg', 'Assets/background.png'],
+        'textFields': ['foreground text']
     },
-    "apns": {
-        "title" : "someTitle",
-        "action-category": "some value",
-        "content-available": true,
-        "action" : "someAction",
-        "url-args" :["args1","arg2"],
-        "localized-title-key" : "some value",
-        "localized-title-arguments" : ["args1","arg2"]
+    'apns': {
+        'title' : 'someTitle',
+        'action-category': 'some value',
+        'content-available': true,
+        'action' : 'someAction',
+        'url-args' :['args1','arg2'],
+        'localized-title-key' : 'some value',
+        'localized-title-arguments' : ['args1','arg2']
     }
 };
 
-//you can also use just the callback pattern
-agSender.Sender( settings ).send( message, options, function( err, response ) {
-    if( err ) {
-        console.log( err );
-        return;
-    }
-    console.log( "success called", response );
+agSender(settings).then((client) => {
+    return client.send(message2, options).then((result) => {
+        console.log(result);
+    });
 });
