97e41f41280112f51d930c565231acde0975c3da
Jose Miguel Gallas Olmedo <josemigallas@gmail.com>
2017-02-21 17:15:08 +0100

    adds example

--- example.js
+++ example.js
@@ -6,7 +6,6 @@ const message = {
     alert: 'Hi',
     sound: 'default',
     badge: 2,
-    simplePush: 'version=123',
     userData: {
         someKey: 'some value',
         anotherCustomKey: 'some other value',
@@ -48,18 +47,71 @@ const message2 = {
         'textFields': ['foreground text']
     },
     'apns': {
-        'title' : 'someTitle',
+        'title': 'someTitle',
         'action-category': 'some value',
         'content-available': true,
-        'action' : 'someAction',
-        'url-args' :['args1','arg2'],
-        'localized-title-key' : 'some value',
-        'localized-title-arguments' : ['args1','arg2']
+        'action': 'someAction',
+        'url-args': ['args1', 'arg2'],
+        'localized-title-key': 'some value',
+        'localized-title-arguments': ['args1', 'arg2']
     }
 };
 
 agSender(settings).then((client) => {
     return client.send(message2, options).then((result) => {
+        console.log(result);
+    });
+});
+
+// A example using the batch method
+const messages = [
+    {
+        message: {
+            alert: 'Hello everybody!',
+        }
+    },
+    {
+        message: {
+            alert: 'Oldies rule!',
+        },
+        options: {
+            criteria: {
+                variants: ['gameBoy', 'vhs', 'spectrum']
+            }
+        }
+    },
+    {
+        message: {
+            'alert': 'This is a very complex message!',
+            'sound': 'default',
+            'badge': 2,
+            'user-data': {
+                'key': 'value',
+                'key2': 'other value'
+            },
+            'windows': {
+                'type': 'tile',
+                'duration': 'short',
+                'badge': 'alert',
+                'tileType': 'TileWideBlockAndText01',
+                'images': ['Assets/test.jpg', 'Assets/background.png'],
+                'textFields': ['foreground text']
+            },
+            'apns': {
+                'title': 'someTitle',
+                'action-category': 'some value',
+                'content-available': true,
+                'action': 'someAction',
+                'url-args': ['args1', 'arg2'],
+                'localized-title-key': 'some value',
+                'localized-title-arguments': ['args1', 'arg2']
+            }
+        }
+    }
+];
+
+agSender(settings).then((client) => {
+    return client.sendBatch(messages).then((result) => {
         console.log(result);
     });
 });
